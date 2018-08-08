3fb8d3de65cdb8ff6e5ba64f998fcc84c4b5ff5b
Ivan Kwiatkowski <justicerage@manalyzer.org>
2018-08-08 14:06:38 +0200

    Updated unit tests to match the changes in hash-library.

--- test/hash-library.cpp
+++ test/hash-library.cpp
@@ -32,7 +32,7 @@ BOOST_AUTO_TEST_CASE(hash_phrase)
 	BOOST_CHECK_EQUAL("9e107d9d372bb6826bd81d3542a419d6", hashes->at(ALL_DIGESTS_MD5));
 	BOOST_CHECK_EQUAL("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", hashes->at(ALL_DIGESTS_SHA1));
 	BOOST_CHECK_EQUAL("d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592", hashes->at(ALL_DIGESTS_SHA256));
-	BOOST_CHECK_EQUAL("4d741b6f1eb29cb2a9b9911c82f56fa8d73b04959d3d9d222895df6c0b28aa15", hashes->at(ALL_DIGESTS_SHA3));
+	BOOST_CHECK_EQUAL("69070dda01975c8c120c3aada1b282394e7f032fa9cf32f4cb2259a0897dfc04", hashes->at(ALL_DIGESTS_SHA3));
 }
 
 // ----------------------------------------------------------------------------
@@ -45,7 +45,7 @@ BOOST_AUTO_TEST_CASE(null_hash)
 	BOOST_CHECK_EQUAL("d41d8cd98f00b204e9800998ecf8427e", hashes->at(ALL_DIGESTS_MD5));
 	BOOST_CHECK_EQUAL("da39a3ee5e6b4b0d3255bfef95601890afd80709", hashes->at(ALL_DIGESTS_SHA1));
 	BOOST_CHECK_EQUAL("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", hashes->at(ALL_DIGESTS_SHA256));
-	BOOST_CHECK_EQUAL("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470", hashes->at(ALL_DIGESTS_SHA3));
+	BOOST_CHECK_EQUAL("a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a", hashes->at(ALL_DIGESTS_SHA3));
 }
 
 // ----------------------------------------------------------------------------
@@ -126,7 +126,7 @@ BOOST_AUTO_TEST_CASE(hash_phrase_file)
     BOOST_CHECK_EQUAL("9e107d9d372bb6826bd81d3542a419d6", hashes->at(ALL_DIGESTS_MD5));
     BOOST_CHECK_EQUAL("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", hashes->at(ALL_DIGESTS_SHA1));
     BOOST_CHECK_EQUAL("d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592", hashes->at(ALL_DIGESTS_SHA256));
-    BOOST_CHECK_EQUAL("4d741b6f1eb29cb2a9b9911c82f56fa8d73b04959d3d9d222895df6c0b28aa15", hashes->at(ALL_DIGESTS_SHA3));
+    BOOST_CHECK_EQUAL("69070dda01975c8c120c3aada1b282394e7f032fa9cf32f4cb2259a0897dfc04", hashes->at(ALL_DIGESTS_SHA3));
 }
 
 // ----------------------------------------------------------------------------
@@ -137,7 +137,7 @@ BOOST_AUTO_TEST_CASE(null_hash_file)
     BOOST_CHECK_EQUAL("d41d8cd98f00b204e9800998ecf8427e", hashes->at(ALL_DIGESTS_MD5));
     BOOST_CHECK_EQUAL("da39a3ee5e6b4b0d3255bfef95601890afd80709", hashes->at(ALL_DIGESTS_SHA1));
     BOOST_CHECK_EQUAL("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", hashes->at(ALL_DIGESTS_SHA256));
-    BOOST_CHECK_EQUAL("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470", hashes->at(ALL_DIGESTS_SHA3));
+    BOOST_CHECK_EQUAL("a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a", hashes->at(ALL_DIGESTS_SHA3));
 }
 
 // ----------------------------------------------------------------------------
