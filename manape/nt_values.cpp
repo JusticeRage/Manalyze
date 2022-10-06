8e77642c911d5d82b5f43b198667ab8c77a88763
Ivan Kwiatkowski <justicerage@manalyzer.org>
2022-10-06 12:12:22 +0200

    Added RICH IDs for VS 17.3.4. Removed CodeQL integration due to lack of support for Boost. Updated the README.md.

--- manape/nt_values.cpp
+++ manape/nt_values.cpp
@@ -752,7 +752,8 @@ const flag_dict COMP_ID_PRODID =
                                 ("VS2022 Update 2 (17.2.2-4) compiler 31329",  0x7A61)
                                 ("VS2022 Update 2 (17.2.5-6) compiler 31332",  0x7A64)
                                 ("VS2022 Update 3 (17.3.0) compiler 31616",    0x7B80)
-                                ("VS2022 Update 3 (17.3.0) compiler 31629",    0x7B8D)
+                                ("VS2022 Update 3 (17.3.0-3) compiler 31629",  0x7B8D)
+                                ("VS2022 Update 3 (17.3.4-5) compiler 31630",  0x7B8E)
     ;
 
 // ----------------------------------------------------------------------------
